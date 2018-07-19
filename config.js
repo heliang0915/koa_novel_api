b519ebd241034befccec00684b5a39217121613c
heiang <598481337@qq.com>
2018-07-19 23:48:31 +0800

    修改小程序ID

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 let config={
     api:{
-        // proxyURL: 'http://novel.juhe.im'
+        // proxyURL: 'http://novel.juhe.im/',
         proxyURL: 'http://59.110.157.243:8020/',
         proxyZhuiShuURL:'http://api.zhuishushenqi.com/', //有几个接口没有包装 这里直接调用追书接口
     },
     wx: {
-        appId: "wx4abeef7a084d03d6",
-        secret: "c6b6640c61f02815312c810f79507778"
+        appId: "wx2097779e598c14f4",
+        secret: "315f6fbb1823e36ebd8131e6c2668178"
     },
     redis:{
         port:6379,
-        host:'127.0.0.1'
-        // host:'www.blogapi.top'
+        // host:'127.0.0.1'
+        host:'www.blogapi.top'
     },
     pageSize:10
 }
